ef2ea8b788856aff3339b3f4d41a0a37db34d56b
breadNbutter42 <97899677+breadNbutter42@users.noreply.github.com>
2022-11-29 23:55:31 -0500

    Add files via upload

---
+++ components/Gallery/DiabloCard.vue
@@ -0,0 +1,62 @@
+<script setup>
+const active = ref(false)
+const selected = useState('selected')
+const searchQuery = useState('search')
+
+const { toggleQuery } = useUtils()
+
+const diabloTraits = [
+  {
+    trait_type: 'Upgraded',
+    traits: [
+      { name: 'Chad/Vial paired', occurrence: 0 }
+    ]
+  },
+  {
+    trait_type: 'Not Yet Upgraded',
+    traits: [
+      { name: 'Not yet paired with a vial', occurrence: 3250 },
+    ]
+  }
+]
+
+const toggleActive = () => active.value = !active.value
+</script>
+
+<template>
+  <div class="mt-2 bg-red-500 dark:bg-red-600 bg-opacity-80 dark:bg-opacity-80 text-white rounded-lg py-2 px-6">
+    <div
+      class="cursor-pointer uppercase tracking-tighter mt-6 mb-3 rounded-xl flex justify-between"
+      @click="toggleActive"
+    >
+      <div>
+        Upgradeable
+      </div>
+      <div class="flex items-center">
+        <div class="text-xs">
+          3250
+        </div>
+        <img
+          src="@/assets/img/arrow.svg"
+          alt=""
+          :class="`w-[24] transition pointer-events-none ${active ? 'rotate-180' : ''}`"
+        />
+      </div>
+    </div>
+    <div
+      :class="`w-full max-h-64 ${active ? 'mb-2 p-2 h-[245px]' : 'h-0'} scrollbar transition-all duration-300 bg-red-200 bg-opacity-50 dark:bg-opacity-50 dark:bg-red-800 px-4 rounded-lg`"
+    >
+      <div v-for="(diablo, id) in diabloTraits" :key="id">
+        <div v-for="trait in diablo.traits" :key="trait.name" :class="`flex items-center mt-2 text-[14px] ${ diablo.trait_type === 'Body' ? 'odd:bg-red-200/[.5] odd:dark:bg-red-900/[.3]' : ''} hover:bg-red-300 hover:dark:bg-red-700 cursor-pointer p-2 rounded-lg`" @click="toggleQuery(diablo.trait_type, trait.name, searchQuery)">
+          <input
+            v-model="selected[trait.name]"
+            :id="`diablo${trait.name}`"
+            type="checkbox"
+            class="appearance-none rounded-md bg-transparent pointer-events-none border border-red-500 dark:border-red-500 p-2 checked:bg-red-500 dark:checked:bg-red-500"
+          />
+          <label :for="`diablo${trait.name}`" class="ml-2 pointer-events-none w-full"><span class="uppercase">{{ diablo.trait_type }}</span>: {{ trait.name }} <span class="float-right text-xs opacity-70">{{ trait.occurrence  }}</span></label>
+        </div>
+    </div>
+    </div>
+  </div>
+</template>
---
+++ components/Layout/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex gap-2 items-center">
+    <div class="pl-4">
+      <NuxtLink to="/" class="text-black hover:text-white dark:text-purple-300 dark:hover:text-purple-100 transition-all font-black uppercase leading-none font-architect tracking-[-0.02em] textshadow">
+        <span class="text-[4vw] md:text-3xl">Chad Doge viewer</span>
+      </NuxtLink>
+      <div class="text-center text-[9px] leading-none text-zinc-400 dark:text-purple-100 hidden sm:flex justify-center gap-2 -mt-1">
+        <div>
+          [OFFICIAL]
+        </div>
+        <div class="uppercase">
+          <a href="https://twitter.com/_ChadDoge_" target="_blank" class="link text-black dark:text-white transition-all">Chad</a> vibes only
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
